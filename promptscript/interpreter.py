1458a8d8d4e6c4a299dac600e626c91e53a17e1e
Zain Javaid <zainmfj@gmail.com>
2024-06-25 13:35:20 -0500

    Name changes

--- promptscript/interpreter.py
+++ promptscript/interpreter.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 from utils.numbers import is_number
 from utils.debug_level import DebugLevel
 
-AST_CONVERSION = {'show':'print_operator', '"':'quote', "'":'quote', '=':'equals', 'load':'load_operator',
+PARSER_CONVERSION = {'show':'print_operator', '"':'quote', "'":'quote', '=':'equals', 'load':'load_operator',
                   'save':'save_to_file', 'chat':'chat_operator', 'draw':'draw_operator', 
                   'listen':'listen_operator', 'if': 'if_conditional', 'elif': 'elif_conditional', 
                   'else': 'else_conditional', 'for': 'for_loop', 'in': 'in_operator', 'while': 'while_loop',
@@ -62,37 +62,37 @@ def lex(command: str, DEBUG_LEVEL: DebugLevel) -> List[str]:
 def parse(command: str, DEBUG_LEVEL: DebugLevel) -> List[Tuple]:
     parts = lex(command, DEBUG_LEVEL)
     
-    ast_operations = []
+    parsed_operations = []
     is_open = False
     for i, part in enumerate(parts):
         try:
             if part.lower() in PROTECTED_BLOCK_CHARACTERS:
                 is_open = not is_open
-                ast_operation = (AST_CONVERSION[part.lower()], part.lower())
+                parse_operation = (PARSER_CONVERSION[part.lower()], part.lower())
             elif is_open:
-                ast_operation = ('msg', part)
+                parse_operation = ('msg', part)
             elif is_number(part) and not is_open:
-                ast_operation = ('num', part)
-            elif not is_open and part.lower() not in AST_CONVERSION:
-                ast_operation = ('var', part)
+                parse_operation = ('num', part)
+            elif not is_open and part.lower() not in PARSER_CONVERSION:
+                parse_operation = ('var', part)
             else:
-                ast_operation = (AST_CONVERSION[part.lower()], part.lower())
+                parse_operation = (PARSER_CONVERSION[part.lower()], part.lower())
                 
-            ast_operations.append(ast_operation)
+            parsed_operations.append(parse_operation)
         except KeyError:
             if DEBUG_LEVEL <= DebugLevel.ERROR:
                 raise SyntaxError(f"SYNTAX ERROR: Unexpected token '{part}'")
             
     if DEBUG_LEVEL <= DebugLevel.DEBUG:
-        print(f'PARSED PARTS :: {ast_operations}')
+        print(f'PARSED PARTS :: {parsed_operations}')
         
-    return ast_operations
+    return parsed_operations
 
 def interpret(command: str, DEBUG_LEVEL: DebugLevel=DebugLevel.INFO) -> str:
-    ast_operations = parse(command, DEBUG_LEVEL)
+    parsed_operations = parse(command, DEBUG_LEVEL)
     
     interpreted_command = ''
-    for op_name, part in ast_operations:
+    for op_name, part in parsed_operations:
         conversion = INTERPRETER_CONVERSION.get(op_name, part)
         interpreted_command += str(conversion)
         
